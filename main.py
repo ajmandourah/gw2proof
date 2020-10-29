c80f709aeec004a914b49b1f2682d8fc5386f8b0
ajmandourah <ajmandourah@gmail.com>
2020-10-29 18:52:28 +0100

    Updating to a new UI

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import base64
-import PySimpleGUI as sg
 import pyperclip
 import keyboard
 import pygetwindow
 import time
+from elevate import elevate
+import webview
+
+
+# elevate(show_console=False)
 
 #defining various killing proof
+fractal = "4f3f0100"
 #w1
 vale = "892f0100"
 gorseval = "b72f0100"
@@ -48,114 +53,87 @@ def getCode(n,kp):
     chat_code = base64.b64encode(bytes.fromhex(li_hexed)).decode()
     chat_code = pyperclip.copy("[&"+str(chat_code)+"]")
 
-sg.theme('Reddit')
-layout = [ 
-    [sg.Text('Choose he KP\LI you want to ping!'),
-    sg.Combo([
-        "Legendary Insight",
-        "W1 Vale Gurdian",
-        "W1 Gorseval",
-        "W1 Sabetha",
-        "W2 Sloth",
-        "W2 Matthaias",
-        "W3 KC",
-        "W3 Xera",
-        "W4 Carin",
-        "W4 Murssat",
-        "W4 Samarog",
-        "W4 Demios",
-        "W5 Desmina",
-        "W5 Statue",
-        "W5 Dhuum",
-        "W6 CA",
-        "W6 Twin",
-        "W6 Qadim",
-        "W7 Sabir",
-        "W7 Adina",
-        "W7 Quadim"
-    ],default_value="Legendary Insight")],
-    [sg.Text('Amount', size =(10, 1)), sg.InputText(default_text=1)], 
-    [sg.Button('Generate', key='-gen-'), sg.Text('how many pings', size =(15, 1)), sg.InputText(default_text=1)]
-]
+def generate(kp, amount, pings):
 
-if __name__ == '__main__':
+    try:
+        amount = int(amount)
+        pings = int(pings)
+    except:
+        window.evaluate_js('alerting(content = "please enter a number between 1 - 250!")') 
+        return
+    if amount > 250 or amount < 1:
+        window.evaluate_js(('alerting("please enter a number between 1 - 250!")')) 
+        return
+    if amount <16:
+        hexed = "0" + str(hex(amount).lstrip("0x").rstrip("L"))
+    else:
+        hexed = hex(amount).lstrip("0x").rstrip("L")
     
-    window = sg.Window('GW2proof by Ladis', layout=layout, size=(400,100), keep_on_top = True, finalize=True)
-    while True:
-        event, values = window.read(timeout=1)
-        if event == None:
-            break
-        if event == '-gen-':
-            try:
-                amount = int(values[1])
-                ping = int(values[2])
-            except:
-                sg.popup("please enter a number between 1 - 250!", keep_on_top= True)
-                continue
-            if amount > 250 or amount < 1 or ping > 5 or ping < 1:
-                sg.popup("please enter a number between 1 - 250! \n Also thr ping amount should not exceed 5 !", keep_on_top= True)
-                continue
-            if amount <16:
-                hexed = "0" + str(hex(amount).lstrip("0x").rstrip("L"))
-            else:
-                hexed = hex(amount).lstrip("0x").rstrip("L")
-            
-            toPing = values[0]
-            if toPing == "Legendary Insight":
-                getCode(hexed, li)
-            if toPing == "W1 Vale Gurdian":
-                getCode(hexed, vale)                
-            if toPing == "W1 Gorseval":
-                getCode(hexed, gorseval)
-            if toPing == "W1 Sabetha":
-                getCode(hexed, sabetha)
-            if toPing == "W2 Sloth":
-                getCode(hexed, sloth)                
-            if toPing == "W2 Matthaias":
-                getCode(hexed, matthias)  
-            if toPing == "W3 KC":
-                getCode(hexed, kc)
-            if toPing == "W3 Xera":
-                getCode(hexed, xera)                
-            if toPing == "W4 Carin":
-                getCode(hexed, carin)
-            if toPing == "W4 Murssat":
-                getCode(hexed, Murssat)
-            if toPing == "W4 Samarog":
-                getCode(hexed, samarog)                
-            if toPing == "W4 Demios":
-                getCode(hexed, demios) 
-            if toPing == "W5 Desmina":
-                getCode(hexed, desmina)
-            if toPing == "W5 Statue":
-                getCode(hexed, statue)                
-            if toPing == "W5 Dhuum":
-                getCode(hexed, dhuum)  
-            if toPing == "W6 CA":
-                getCode(hexed, ca)
-            if toPing == "W6 Twin":
-                getCode(hexed, twin)                
-            if toPing == "W6 Qadim":
-                getCode(hexed, qadim)
-            if toPing == "W7 Sabir":
-                getCode(hexed, sabir)
-            if toPing == "W7 Adina":
-                getCode(hexed, adina)                
-            if toPing == "W7 Quadim":
-                getCode(hexed, qadim2) 
+    # li_hexed = "02"+str(hexed)+"f62d0100"
+    # chat_code = base64.b64encode(bytes.fromhex(li_hexed)).decode()
+    # chat_code = pyperclip.copy("[&"+str(chat_code)+"]")
 
-            try:
-                gw2 = pygetwindow.getWindowsWithTitle("Guild Wars 2")[0]
-            except:
-                sg.popup("It seems like Guild wars 2 is not running!", keep_on_top= True)
-                continue
-            gw2.activate()
-            time.sleep(0.5)
-            keyboard.press_and_release('esc')
-            for _ in range(ping):
-                paste()
+    toPing = str(kp)
+    if toPing == "Legendary Insight":
+        getCode(hexed, li)
+    if toPing == "W1 Vale Gurdian":
+        getCode(hexed, vale)                
+    if toPing == "W1 Gorseval":
+        getCode(hexed, gorseval)
+    if toPing == "W1 Sabetha":
+        getCode(hexed, sabetha)
+    if toPing == "W2 Sloth":
+        getCode(hexed, sloth)                
+    if toPing == "W2 Matthaias":
+        getCode(hexed, matthias)  
+    if toPing == "W3 KC":
+        getCode(hexed, kc)
+    if toPing == "W3 Xera":
+        getCode(hexed, xera)                
+    if toPing == "W4 Carin":
+        getCode(hexed, carin)
+    if toPing == "W4 Murssat":
+        getCode(hexed, Murssat)
+    if toPing == "W4 Samarog":
+        getCode(hexed, samarog)                
+    if toPing == "W4 Demios":
+        getCode(hexed, demios) 
+    if toPing == "W5 Desmina":
+        getCode(hexed, desmina)
+    if toPing == "W5 Statue":
+        getCode(hexed, statue)                
+    if toPing == "W5 Dhuum":
+        getCode(hexed, dhuum)  
+    if toPing == "W6 CA":
+        getCode(hexed, ca)
+    if toPing == "W6 Twin":
+        getCode(hexed, twin)                
+    if toPing == "W6 Qadim":
+        getCode(hexed, qadim)
+    if toPing == "W7 Sabir":
+        getCode(hexed, sabir)
+    if toPing == "W7 Adina":
+        getCode(hexed, adina)                
+    if toPing == "W7 Quadim":
+        getCode(hexed, qadim2) 
+    if toPing == "Fractal of the Mist CM":
+        getCode(hexed, fractal)
 
-           
+    try:
+        gw2 = pygetwindow.getWindowsWithTitle("Guild Wars 2")[0]
+    except:
+        window.evaluate_js("alerting('It seems like Guild wars 2 is not running!')")
+    gw2.activate()
+    time.sleep(0.5)
+    keyboard.press_and_release('esc')
+    for _ in range(pings):
+        paste()
+    window.evaluate_js('alerting("Success!")') 
+    return
+
+window = webview.create_window("GW2Proof",url='web/main.html',width=600, height=550, resizable=False, easy_drag=False)
+window.expose(generate)
+webview.start(gui="mshtml")
 
 
                 
